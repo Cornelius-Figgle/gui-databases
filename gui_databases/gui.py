379f530bcdb1dfbf44952362ac81fac27c260e84
Cornelius-Figgle <max@fullimage.net>
2025-05-01 13:51:14 +0100

    Basic GUI layout

--- gui_databases/gui.py
+++ gui_databases/gui.py
@@ -22,7 +22,11 @@ from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
     QWidget,
-    QGridLayout
+    QGridLayout,
+    QLabel, 
+    QLineEdit,
+    QHBoxLayout,
+    QPushButton
 )
 
 
@@ -79,10 +83,73 @@ class GUI_Interface(QMainWindow):
 
     def login(self) -> None:
         '''
-            
+        Processes the login event.
         '''
 
-        ...
+        # stores all the widgets for this screen
+        widget_dict = dict()
+
+        # creates a title and adds it to the window's layout
+        widget_dict['title_text'] = QLabel('<h1>Login to the database</h1>')
+        self.layout_current.addWidget(widget_dict['title_text'], 0, 0)
+
+        # grid to hold the labels and their input boxes
+        widget_dict['cred_grid_widget'] = QWidget()
+        widget_dict['cred_grid_layout'] = QGridLayout()
+        
+        widget_dict['username_label'] = QLabel('Username: ')
+        widget_dict['username_input'] = QLineEdit()
+        widget_dict['password_label'] = QLabel('Password: ')
+        widget_dict['password_input'] = QLineEdit()
+        widget_dict['password_input'].setEchoMode(QLineEdit.EchoMode.Password)
+        
+        widget_dict['cred_grid_layout'].addWidget(
+            widget_dict['username_label'],
+            0, 0
+        )
+        widget_dict['cred_grid_layout'].addWidget(
+            widget_dict['username_input'],
+            0, 1
+        )
+        widget_dict['cred_grid_layout'].addWidget(
+            widget_dict['password_label'],
+            1, 0
+        )
+        widget_dict['cred_grid_layout'].addWidget(
+            widget_dict['password_input'],
+            1, 1
+        )
+        
+        widget_dict['cred_grid_widget'].setLayout(
+            widget_dict['cred_grid_layout']
+        )
+
+        self.layout_current.addWidget(widget_dict['cred_grid_widget'], 1, 0)
+
+        # hbox to buffer the button to the right
+        widget_dict['confirm_hbox_widget'] = QWidget()
+        widget_dict['confirm_hbox_layout'] = QHBoxLayout()
+
+        widget_dict['confirm_hbox_layout'].addWidget(
+            QWidget()
+        )
+        widget_dict['confirm_hbox_layout'].addWidget(
+            QWidget()
+        )
+
+        widget_dict['confirm_button'] = QPushButton('Login')
+        widget_dict['confirm_hbox_layout'].addWidget(
+            widget_dict['confirm_button']
+        )
+
+        widget_dict['confirm_hbox_widget'].setLayout(
+            widget_dict['confirm_hbox_layout']
+        )
+        
+        self.layout_current.addWidget(widget_dict['confirm_hbox_widget'], 2, 0)
+
+        # set the layout
+        self.layout_widget.setLayout(self.layout_current)
 
         return
 
